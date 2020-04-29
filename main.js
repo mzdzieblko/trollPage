82a7ee597698eb8d6c8a370f6940ad3e4a7c8b48
Mateusz Zdziebłko <zdzieblko@icloud.com>
2020-04-29 21:44:40 +0200

    feat: JS test

---
+++ main.js
@@ -0,0 +1,22 @@
+alert("Witaj, przybyszu. Zajrzyj w konsolę ;D");
+
+const name = "Mateusz";
+const age = 31;
+
+console.log(`Nazwam się ${name},
+mam ${age} lat
+i zaczynam lubić JS`);
+
+alert("Zastanawia mnie...");
+alert("jedna ");
+alert("rzecz");
+alert("...");
+alert("Kiedy");
+alert("przestaniesz");
+alert("klikać w te okienka ");
+alert("");
+alert("Pozdro dla wytrwałych ;)");
+alert("i cierpliwych :D");
+alert("PS...");
+alert("mnie też się ta strona nie podoba ;)");
+alert("test");
